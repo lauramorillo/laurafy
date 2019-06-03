2cbf1f599670615868330a8a06f727a30130a44b
Laura Morillo-Velarde <laura.morillovelarde@gmail.com>
2019-06-03 20:43:30 +0200

    Adding static endpoint to serve songs.

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/play', express.static(path.join(__dirname, 'songs')));
 
 app.use('/', indexRouter);
 
