214d438367672ecba50f89c10c8ffabb78300bf2
Laura Morillo-Velarde <laura.morillovelarde@gmail.com>
2019-06-19 22:42:55 +0200

    Handling cors.

--- backend/config/server.js
+++ backend/config/server.js
@@ -3,7 +3,8 @@ const path = require('path');
 const logger = require('morgan');
 
 const indexRouter = require('../routes/routes');
-const ErrorHandler = require('./error_handler')
+const ErrorHandler = require('./error_handler');
+const AccessControl = require('./access_control');
 
 module.exports.start = () => {
   const app = express();
@@ -12,6 +13,7 @@ module.exports.start = () => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
 
+  app.use(AccessControl.handler)
   app.use('/play', express.static(path.join(__dirname, '../songs')));
   app.use('/', indexRouter);
 
